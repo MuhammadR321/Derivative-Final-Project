5ae9725ba851d199b9d50b9e03fd86e785394db9
MuhammadR321 <90729610+MuhammadR321@users.noreply.github.com>
2021-12-11 11:32:54 -0500

    Create derivative.h

---
+++ derivative.h
@@ -0,0 +1,11 @@
+#ifndef DERIVATIVE_H
+#define DERIVATIVE_H
+
+int fxntype;
+char fxnvar; 
+int i, linearvals[2];
+const char *placeholders[2];
+int j, linearvals2[4];
+const char *placeholders2[4];
+
+#endif 
