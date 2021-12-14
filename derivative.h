080ba2738c4c6179b150df889bb6e6223623cfdd
MuhammadR321 <90729610+MuhammadR321@users.noreply.github.com>
2021-12-14 16:41:40 -0500

    Update derivative.h

--- derivative.h
+++ derivative.h
@@ -1,11 +1,18 @@
+/*Muhammad Rashid*/
+
 #ifndef DERIVATIVE_H
 #define DERIVATIVE_H
 
 int fxntype;
 char fxnvar; 
-int i, linearvals[2];
+int i;
+float linearvals[2];
 const char *placeholders[2];
-int j, linearvals2[4];
+int j;
+float binomvals[4];
 const char *placeholders2[4];
+int k;
+float trinomvals[6];
+const char *placeholders3[6];
 
-#endif 
+#endif
